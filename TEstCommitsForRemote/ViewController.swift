f3e827e6b56b118cd53df748436b02642cc1e091
kost9klinov <kost9klinov>
2019-02-16 11:29:53 +0300

    Initial Commit

---
+++ TEstCommitsForRemote/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TEstCommitsForRemote
+//
+//  Created by Константин Клинов on 2/16/19.
+//  Copyright © 2019 Константин Клинов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
